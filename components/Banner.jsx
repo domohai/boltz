8618cfc0645dd1271ac948ee5caf065de34bc1b2
DucHiepisme <113037834+DucHiepisme@users.noreply.github.com>
2024-10-21 11:41:59 +0700

    Updating

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className="banner">
-      <div className="banner-image">
-        <img src="/assets/images/banner.jpg" alt="BoltZ Background" className="w-full h-full object-cover"  />
-      </div>
-    </div>
+    <div className="w-screen h-screen overflow-hidden">
+    <img 
+      src="/assets/images/banner.jpg" 
+      alt="BoltZ Background" 
+      className="w-full h-full object-cover" 
+    />
+  </div>
   );
 };
 
