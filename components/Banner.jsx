6adc30bf4a02cc4fd84420402ef512b0cf160f69
DucHiepisme <113037834+DucHiepisme@users.noreply.github.com>
2024-10-23 09:49:06 +0700

    Updating slogan border and shadow

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -11,14 +11,11 @@ const Banner = () => {
         />
     </div>
     <div className="absolute inset-y-0 mt-12 left-0 w-1/2 h-1/2 flex items-center justify-center">
-    <div className="bg-white py-8 px-6 rounded-lg shadow-lg">
+    <div className="bg-white py-8 px-6">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">FAST & FREE DELIVERY</h1>
       <p className="text-gray-600 mb-6">
         Speedy Delivery, Right to Your Door!
       </p>
-      <button className="bg-orange-500 hover:bg-orange-600 text-white font-medium py-3 px-6 rounded-md">
-        READ MORE
-      </button>
     </div>
     </div>
 </div>
