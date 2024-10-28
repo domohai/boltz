4c452b87fb081fa511d3a0c4347e07e70519373b
domohai <domohai12@gmail.com>
2024-10-28 12:24:26 +0700

    modified manage accounts screen of leader

--- app/api/users/[id]/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET(_, response) {
-  console.log(response.params.id);
-  return NextResponse.json({ message: "Hello from the server!" });
-}
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,8 +1,13 @@
 import { NextResponse } from "next/server";
 import { createConnection } from "@utils/db.js";
-import { users } from "@utils/users_data.js";
+import { leader } from "@utils/users_data/users_data.js";
 
-export function GET() {
-  const data = users;
-  return NextResponse.json({ data });
+export async function POST(req, res) {
+  let data = await req.json();
+  
+  if (!data.email || !data.password || !data.role) {
+    return NextResponse.json({ message: "Required field is empty", ok: false }, { status: 400 });
+  }
+
+  return NextResponse.json({ res: "Successfully add users", ok: true }, { status: 201 });
 }
