2653f5ac9867754f606b00c13bc9addf0096b3ea
Hai <duchaisub01@gmail.com>
2024-11-08 22:14:56 +0700

    Minor update

--- app/api/leader/collection_manager/[id]/route.js
+++
@@ -1,6 +0,0 @@
-import { handleDeleteCollectionManagerById } from "@back-end/controllers/cmController.js";
-
-export async function DELETE(req, { params }) {
-  const { id } = params;
-  return handleDeleteCollectionManagerById(req, id);
-}
--- app/api/leader/service_manager/route.js
+++
@@ -1,5 +0,0 @@
-import { handleGetAllServiceManagers } from "@back-end/controllers/smController.js";
-
-export async function GET(req, res) {
-  return handleGetAllServiceManagers(req, res);
-}
