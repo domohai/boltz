dc0a947b94e195d2e00aabc39fd96f7b094bb10c
Hai <duchaisub01@gmail.com>
2024-11-11 23:46:11 +0700

    Fixed bug null accounts value && Update display collection managers manage accounts

--- app/(management)/collection_manager/manage_accounts/page.jsx
+++ app/(management)/collection_manager/manage_accounts/page.jsx
@@ -6,6 +6,7 @@ import { useSession } from 'next-auth/react';
 import collectionManagerAccounts from "./collectionManagerAccounts";
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/modal";
 import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from "@nextui-org/table";
+import { Tooltip } from "@nextui-org/tooltip";
 
 const Page = () => {
   const {data: session, status} = useSession();
@@ -24,7 +25,7 @@ const Page = () => {
     const data = await res.json();
     if (data.ok) {
       setAccounts(data.users);
-      console.log(accounts);
+      console.log(data.users);
     } else {
       console.log(data.message);
       alert('Failed to get staff accounts');
@@ -32,11 +33,10 @@ const Page = () => {
   };
 
   useEffect(() => {
-    if (session) {
+    if (session && collection_point_id) {
       getStaffAccounts();
     }
-    
-  }, []);
+  }, [session, collection_point_id]);
 
   const addManagerHandler = async (e) => {
     e.preventDefault();
@@ -49,6 +49,7 @@ const Page = () => {
     setPassword('');
     setRole('');  
   };
+
 
   return (
     <div className="w-full p-6 bg-white min-h-screen">
@@ -94,9 +95,35 @@ const Page = () => {
             <TableColumn>Họ và tên</TableColumn>
             <TableColumn>Email</TableColumn>
             <TableColumn>Role</TableColumn>
+            <TableColumn>Actions</TableColumn>
           </TableHeader>
-          <TableBody>
-            
+          <TableBody items={accounts}>
+              {(item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.role}</TableCell>
+                <TableCell>
+                  <div className="flex">
+                    {/* Delete icon */}
+                    <Tooltip content="Xoá" placement="top" color='danger'>
+                      <Button 
+                        size="sm" 
+                        isIconOnly
+                        color='danger'
+                        variant="light" 
+                        className="h-8 w-8 p-0 text-gray-400"
+                        onPress={() => openDeleteModal(item.id)}
+                        startContent={
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                          </svg>
+                        }></Button>
+                    </Tooltip>
+                  </div>
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </div>
