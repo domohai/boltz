29ea042ca22e3febaf958be517d38241a21163ec
Hai <duchaisub01@gmail.com>
2024-11-17 11:51:48 +0700

    Add DELETE method for collection manager

--- app/api/collection_manager/user/route.js
+++ app/api/collection_manager/user/route.js
@@ -3,3 +3,7 @@ import { handleGetStaffOfCP } from "@back-end/controllers/userController.js";
 export async function GET(req, res) {
     return handleGetStaffOfCP(req, res);
 }
+
+export async function POST(req, res) {
+  return handleAddUser(req, res);
+}
